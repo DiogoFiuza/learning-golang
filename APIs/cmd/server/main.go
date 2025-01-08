be63566ddb1800b7e06dea76c6d80ac922e6877b
DiogoFiuza <diogo.fiuza@mercadolivre.com>
2025-01-08 20:09:58 -0300

    feat: create product handler

--- APIs/cmd/server/main.go
+++ APIs/cmd/server/main.go
@@ -1,9 +1,30 @@
 package main
 
-import "github.com/DiogoFiuza/learning-golang/APIs/configs"
+import (
+	"github.com/DiogoFiuza/learning-golang/APIs/configs"
+	"github.com/DiogoFiuza/learning-golang/APIs/internal/entity"
+	"github.com/DiogoFiuza/learning-golang/APIs/internal/infra/database"
+	"github.com/DiogoFiuza/learning-golang/APIs/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"net/http"
+)
 
 func main() {
-	config, _ := configs.LoadConfig(".")
-	println(config.DBDriver)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
 
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":8000", nil)
 }
